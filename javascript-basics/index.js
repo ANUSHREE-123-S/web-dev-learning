3b27c79db85eeca238d092e58615bf2670b0b8f4
ANUSHREE-123-S <anushreeshivalinga@gmail.com>
2026-04-21 20:42:08 +0530

    Initial structure for web dev learning

---
+++ javascript-basics/index.js
@@ -0,0 +1,13 @@
+// Variables
+let name = "Anushree";
+let age = 20;
+const isStudent = true;
+
+// Output
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// Changing value
+age = 21;
+console.log("Updated age:", age);
