e305fb2b947a7991c6d92796b3ea636f0705834b
ANUSHREE-123-S <anushreeshivalinga@gmail.com>
2026-04-24 18:40:58 +0530

    Day 4: Objects, Destructuring, Spread

--- javascript-basics/day3-arrays/code.js
+++ javascript-basics/day3-arrays/code.js
@@ -1,16 +1,16 @@
 // Day 3: Arrays + map()
 
-// Task 1
+// Task 1 Use map() → multiply by 2
 const nums = [2, 4, 6, 8];
 const doubled = nums.map((n) => n * 2);
 console.log(doubled);
 
-// Task 2
+// Task 2  vConvert numbers to strings
 const numbers = [1, 2, 3];
 const strings = numbers.map((n) => n.toString());
 console.log(strings);
 
-// Task 3
+// Task 3  Use map():If marks > 40 → "Pass"  Else → "Fail"
 const users = [
   { name: "Anu", marks: 50 },
   { name: "Ram", marks: 30 }
